438bf499976c9b88821d5767238b09c23a9e6ad1
Kilgore Trout <114642646+kilgortrout42@users.noreply.github.com>
2024-05-16 23:15:43 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'musing-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/index.html'
+    // Add any other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
